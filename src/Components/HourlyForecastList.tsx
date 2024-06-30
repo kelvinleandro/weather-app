febae5277eb4dcfb30ddb07f91c7bfd0bf067b8d
kelvinleandro <kelvinleandro5@gmail.com>
2024-06-30 17:13:29 -0300

    feat(HourlyForecastList): add droplet icon next to humidity value

--- src/Components/HourlyForecastList.tsx
+++ src/Components/HourlyForecastList.tsx
@@ -8,6 +8,8 @@ import {
 import React from "react";
 import { HourlyForecast } from "@/types/weatherApi";
 import { FlatList } from "react-native-gesture-handler";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+
 
 type Props = {
   data: HourlyForecast[] | undefined;
@@ -23,9 +25,12 @@ const HourlyForecastList = ({ data }: Props) => {
     return (
       <View style={styles.itemContainer}>
         <Text style={styles.textColor}>{time}</Text>
-        <Image source={{ uri: "https:" + item.condition.icon }} style={styles.icon} />
+        <Image source={{ uri: "https:" + item.condition.icon }} style={styles.weatherIcon} />
         <Text style={styles.textColor}>{item.temp_c}°</Text>
-        <Text style={styles.textColor}>{item.humidity}%</Text>
+        <View style={styles.humiditySection}>
+          <FontAwesome6 name="droplet" size={14} color="rgba(255,255,255, 0.4)" />
+          <Text style={styles.textColor}>{item.humidity}%</Text>
+        </View>
       </View>
     );
   };
@@ -46,13 +51,19 @@ export default HourlyForecastList;
 const styles = StyleSheet.create({
   itemContainer: {
     alignItems: "center",
+    gap: 4,
   },
   textColor: {
     color: "white",
   },
-  icon: {
+  weatherIcon: {
     width: 64,
     height: 64,
     resizeMode: "contain",
   },
+  humiditySection: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 2,
+  }
 });
