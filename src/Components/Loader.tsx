22bf0e045190faf7b7053b331f16c9d9f6462448
kelvinleandro <kelvinleandro5@gmail.com>
2024-07-04 10:05:18 -0300

    feat: update loading and splash screen

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -1,9 +1,34 @@
-import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { useEffect, useRef } from "react";
+import { StyleSheet, View } from "react-native";
+import LottieView from "lottie-react-native";
 
 const Loader = () => {
+  const animation = useRef<LottieView>(null);
+
+  useEffect(() => {
+    const loopAnimation = () => {
+      if (animation.current) {
+        animation.current.play(93, 160);
+      }
+    };
+
+    loopAnimation();
+
+    const interval = setInterval(() => {
+      loopAnimation();
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="large" color="#7cb0d3" />
+      <LottieView
+        ref={animation}
+        loop={false}
+        style={styles.animation}
+        source={require("@/assets/loader.json")}
+      />
     </View>
   );
 };
@@ -16,5 +41,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#303968",
+  },
+  animation: {
+    width: 250,
+    height: 250,
   },
 });
